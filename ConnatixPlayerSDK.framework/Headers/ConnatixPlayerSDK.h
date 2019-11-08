3a4ca08b66a591f23571b4bf7fe75cfa6716807a
Mihai Fischer <mihai.fischer@connatix.com>
2019-11-08 15:08:47 +0200

    1.0.9

---
+++ ConnatixPlayerSDK.framework/Headers/ConnatixPlayerSDK.h
@@ -0,0 +1,19 @@
+//
+//  PlayerSDK.h
+//  PlayerSDK
+//
+//  Created by Mihai Fischer on 11/02/2019.
+//  Copyright © 2019 Mihai Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayerSDK.
+FOUNDATION_EXPORT double ConnatixPlayerSDKVersionNumber;
+
+//! Project version string for PlayerSDK.
+FOUNDATION_EXPORT const unsigned char ConnatixPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerSDK/PublicHeader.h>
+
+
